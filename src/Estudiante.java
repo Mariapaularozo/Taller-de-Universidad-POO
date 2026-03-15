0127d20ab9dd83614ac16c9efbc067e3fab08c5d
Susana Marin B <randommuffin.0@gmail.com>
2026-03-15 13:10:19 -0500

    Add files via upload

---
+++ src/Estudiante.java
@@ -0,0 +1,32 @@
+Package pre_requisitos;
+public class Estudiante {
+    private String nombre;
+    private String[] materiasAprobadas;
+    private double notaFinal;
+
+    public Estudiante(String nombre, String[] materiasAprobadas, double notaFinal) {
+        this.nombre = nombre;
+        this.materiasAprobadas = materiasAprobadas;
+        this.notaFinal = notaFinal;
+    }
+
+    public String getNombre() { return nombre; }
+    public double getNotaFinal() { return notaFinal; }
+
+    public boolean puedeInscribirse(Curso curso) {
+        for (int i = 0; i < materiasAprobadas.length; i++) {
+            if (materiasAprobadas[i].equals(curso.getPrerequisito())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void verificarNota() {
+        if (notaFinal < 2) {
+            System.out.println(nombre + " reprobó por ley del dos. Nota: " + notaFinal);
+        } else {
+            System.out.println(nombre + " aprobó el curso. Nota: " + notaFinal);
+        }
+    }
+}
